d7fb567a7a9526e08febbb4efd1b61e4d3e298f6
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-03-21 20:55:10 +0100

    basic folder structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>gnNews</div>
+  )
+}
+
+export default App
---
+++ src/components/NewsListItem/NewsListItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsListItem = () => {
+  return (
+    <div>NewsListItem</div>
+  )
+}
+
+export default NewsListItem
