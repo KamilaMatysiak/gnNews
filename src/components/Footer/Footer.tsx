9dbbd9b3ad0fa0b781d8cdc1028f517a561b6b63
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-03-23 16:49:25 +0100

    add timer to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Footer.scss'
 
 const Footer = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    var timer = setInterval(() => setTime(new Date()), 1000)
+  
+    return function cleanup() {
+      clearInterval(timer);
+    }
+  })
+  
+  const getTime = () => {
+    const hour = time.getHours()
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    return `${hour}:${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+  }
+
   return (
     <div className='footer'>
-      <p className='footer__time'>00:00</p>
+      <p className='footer__time'>{getTime()}</p>
       <p className='footer__articles'>Liczba artykułów: </p>
     </div>
   )
