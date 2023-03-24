1a861594d61c227021f1afb404e3d82a75c18be8
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-03-24 15:33:44 +0100

    better modal view, darkmode scrollbar

--- src/components/FeedbackModal/FeedbackModal.tsx
+++ src/components/FeedbackModal/FeedbackModal.tsx
@@ -2,15 +2,37 @@ import React from 'react'
 import { Dialog } from '@mui/material'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import { toggleModal } from '../../features/feedbackSlice'
+import CloseIcon from '@mui/icons-material/Close';
+import './FeedbackModal.scss'
 
 const FeedbackModal = () => {
     const open = useAppSelector((state) => state.feedbackModal.open)
+    const lightMode = useAppSelector((state) => state.lightMode.mode)
     const dispatch = useAppDispatch();
 
+
   return (
-    <Dialog open={open} onClose={()=>dispatch(toggleModal())}>
-        <p>Happy</p>
-        <p>Not happy</p>
+    <Dialog open={open} onClose={()=>dispatch(toggleModal())} className="modal">
+        <div className={`modal__header modal__header${lightMode === "dark" && '--dark'}`}>
+            <h1 className='modal__title'>Feedback</h1>
+            <CloseIcon className='modal__icon' onClick={()=>dispatch(toggleModal())}/>
+        </div>
+        <div className={`modal__content modal__content${lightMode === "dark" && '--dark'}`}>
+            <div className='likes-container'>
+                <h1 className='likes-container__header'>Easy</h1>
+                <ul className='likes-container__list'>
+                    <li className='likes-container__feedback'>Designin an app</li>
+                    <li className='likes-container__feedback'>Implementing the UI</li>
+                </ul>
+            </div>
+            <div className='dislikes-container'>
+                <h1 className='dislikes-container__header'>Hard</h1>
+                <ul className='dislikes-container__list'>
+                    <li className='dislikes-container__feedback'>Figuring out how does Redux work in Typescript <i>(usually I use Javascript)</i></li>
+                    <li className='dislikes-container__feedback'>Testing</li>
+                </ul>
+            </div>
+        </div>
     </Dialog>
   )
 }
