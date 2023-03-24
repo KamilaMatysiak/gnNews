cb786e718e77a8dc258b6199e9e317bad162d070
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-03-24 22:44:34 +0100

    change language of countries

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { fetchCountries, CountryType } from '../../features/countriesSlice'
 
 const Sidebar = () => {
   const countries = useAppSelector((state) => state.countries)
+  const language = useAppSelector((state) => state.language.language)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -15,9 +16,9 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className='sidebar__content'>
       {countries.countries.map((country : CountryType, i) => (
-        <a key={i} className='country-item' href={`/country/${country.name.common}`}>
+        <a key={i} className='country-item' href={`/country/${country.cca2}`}>
           <img className="country-item__img" src={country.flags.svg}/> 
-          <p className="country-item__name">{country.name.common}</p>
+          <p className="country-item__name">{language === "pl" ? country.translations.pol.common : country.name.common}</p>
         </a>
       ))}
       </div>
